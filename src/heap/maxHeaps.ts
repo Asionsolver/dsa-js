6f77ce2008e09c43bcb73b043995bf1ec82ce936
asionsolver <asionsolver274@gmail.com>
2025-11-23 11:57:09 +0600

    implement Max Heap  remove method in TypeScript

--- src/heap/maxHeaps.ts
+++ src/heap/maxHeaps.ts
@@ -41,6 +41,58 @@ class MaxHeap {
   print() {
     console.log(this.heap);
   }
+
+  heapIfyDown(index: number) {
+    let largest = index;
+
+    let leftChild = this.getLeftChild(index);
+    let rightChild = this.getRightChild(index);
+
+    if (
+      leftChild < this.heap.length &&
+      this.heap[leftChild] > this.heap[largest]
+    ) {
+      largest = leftChild;
+    }
+
+    if (
+      rightChild < this.heap.length &&
+      this.heap[rightChild] > this.heap[largest]
+    ) {
+      largest = rightChild;
+    }
+
+    if (largest !== index) {
+      this.swap(largest, index);
+      this.heapIfyDown(largest);
+    }
+  }
+
+  remove() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+
+    let firstValue = this.heap[0];
+    this.heap[0] = this.heap.pop()!;
+    this.heapIfyDown(0);
+    return firstValue;
+  }
+
+  peek() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+
+    return this.heap[0];
+  }
+
+  size() {
+    return this.heap.length;
+  }
 }
 
 const maxHeap = new MaxHeap();
@@ -52,3 +104,7 @@ maxHeap.inset(50);
 maxHeap.inset(40);
 maxHeap.inset(60);
 maxHeap.print();
+console.log(maxHeap.remove());
+maxHeap.print();
+console.log(maxHeap.peek());
+console.log(maxHeap.size());
