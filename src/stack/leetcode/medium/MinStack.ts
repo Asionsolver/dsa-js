b7fa099277b2667499ee2eec4b7f227bd4901dd8
asionsolver <asionsolver274@gmail.com>
2026-04-23 23:49:12 +0600

    solve leetcode 155. Min Stack

---
+++ src/stack/leetcode/medium/MinStack.ts
@@ -0,0 +1,78 @@
+// 155. Min Stack
+
+/**
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+*/
+
+class MinStack {
+  private stack: number[];
+  private minStack: number[];
+
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+
+  push(val: number): void {
+    this.stack.push(val);
+
+    // If minStack is empty, the new value is inherently the minimum.
+    if (this.minStack.length === 0) {
+      this.minStack.push(val);
+    } else {
+      // Otherwise, push the smaller value between the current val and the previous minimum
+      const currentMin = this.minStack[this.minStack.length - 1];
+      this.minStack.push(Math.min(currentMin, val));
+    }
+  }
+
+  pop(): void {
+    this.stack.pop();
+    this.minStack.pop();
+  }
+
+  top(): number {
+    return this.stack[this.stack.length - 1];
+  }
+
+  getMin(): number {
+    return this.minStack[this.minStack.length - 1];
+  }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+// Test cases
+const minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+console.log(minStack.getMin()); // Output: -3
+minStack.pop();
+console.log(minStack.top()); // Output: 0
+console.log(minStack.getMin()); // Output: -2
