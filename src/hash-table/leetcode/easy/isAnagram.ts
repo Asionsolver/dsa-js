169a532ee5da346d52c8415be455d77267849227
asionsolver <asionsolver274@gmail.com>
2026-08-11 20:17:18 +0600

    solve  leetcode 242. Valid Anagram using letter frequency counter

--- src/hash-table/leetcode/easy/isAnagram.ts
+++ src/hash-table/leetcode/easy/isAnagram.ts
@@ -31,31 +31,56 @@ s and t consist of lowercase English letters.
 Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
  */
 
+// Approach 1: Using a map to keep track of letter frequencies
+// function isAnagram(s: string, t: string): boolean {
+//   // 1. They can never be anagrams if they are not of equal length
+//   if (s.length !== t.length) {
+//     return false;
+//   }
+
+//   // 2. Creating a map to keep track of letter frequencies
+//   const charMap = new Map<string, number>();
+
+//   // 3. Count the number of characters in the string 's'
+//   for (const char of s) {
+//     charMap.set(char, (charMap.get(char) || 0) + 1);
+//   }
+
+//   // 4. Match the characters in the string 't'
+//   for (const char of t) {
+//     let count = charMap.get(char);
+//     // If the character is not in the map or its count is already 0
+//     if (!count) {
+//       return false;
+//     }
+
+//     // Decrement count by 1
+
+//     charMap.set(char, count - 1);
+//   }
+//   return true;
+// }
+
+// Approach 2: Using an array to keep track of letter frequencies
 function isAnagram(s: string, t: string): boolean {
   // 1. They can never be anagrams if they are not of equal length
   if (s.length !== t.length) {
     return false;
   }
 
-  // 2. Creating a map to keep track of letter frequencies
-  const charMap = new Map<string, number>();
+  // Array filled with 0s for 26 English characters
+  const counter = new Array<number>(26).fill(0);
 
-  // 3. Count the number of characters in the string 's'
-  for (const char of s) {
-    charMap.set(char, (charMap.get(char) || 0) + 1);
+  for (let i = 0; i < s.length; i++) {
+    // Extract the index (0 to 25) by omitting the ASCII code of 'a'
+    counter[s.charCodeAt(i) - 97]++;
+    counter[t.charCodeAt(i) - 97]--;
   }
-
-  // 4. Match the characters in the string 't'
-  for (const char of t) {
-    let count = charMap.get(char);
-    // If the character is not in the map or its count is already 0
-    if (!count) {
+  // Check if the value at an index is greater than or less than 0
+  for (const count of counter) {
+    if (count !== 0) {
       return false;
     }
-
-    // Decrement count by 1
-
-    charMap.set(char, count - 1);
   }
   return true;
 }
@@ -63,6 +88,7 @@ function isAnagram(s: string, t: string): boolean {
 // Test Case:
 // Test Case 1: Simple anagram
 console.log(isAnagram("anagram", "nagaram")); // Output: true
+console.log(isAnagram("listen", "silent")); // Output: true
 
 // Test Case 2: Different words
 console.log(isAnagram("rat", "car")); // Output: false
@@ -71,7 +97,7 @@ console.log(isAnagram("rat", "car")); // Output: false
 console.log(isAnagram("hello", "hell")); // Output: false
 
 // Test Case 4: Unicode (Bangla characters)
-console.log(isAnagram("বাংলা", "লাবাং")); // Output: true
+// console.log(isAnagram("বাংলা", "লাবাং")); // Output: true
 
 // Test case 5: Same character but different times used
 console.log(isAnagram("aa", "a")); // Output: false
